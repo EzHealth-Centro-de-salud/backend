225644b6938fa40ab4a0e5b9cfa35985140deb64
Space_Dog0 <nicolas.bravo@alumnos.ucn.cl>
2024-05-09 20:23:50 -0400

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,7 +21,11 @@ dotenv.config();
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: process.env.DATABASE_URL,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
