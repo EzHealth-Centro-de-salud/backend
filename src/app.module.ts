e2d1ecce7ec0a6709255ece845af216ec954f674
DanielSanAI <daniel.barrios@alumnos.ucn.cl>
2024-05-10 21:19:14 -0400

    updated URL

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,11 +21,7 @@ dotenv.config();
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
+      url: process.env.DATABASE_URL,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
