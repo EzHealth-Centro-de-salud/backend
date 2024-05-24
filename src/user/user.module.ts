e54723b671950c265ad9e5c48da542f992783859
Space_Dog0 <nicolas.bravo@alumnos.ucn.cl>
2024-05-24 00:52:43 -0400

    fix: branch user relations

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Patient, Personnel } from './entities/user.entity';
-import { UserResolver } from './user.resolver';
+import { PatientResolver, PersonnelResolver } from './user.resolver';
 import { UserService } from './user.service';
 import { BranchModule } from 'src/branch/branch.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Patient, Personnel]), BranchModule],
-  providers: [UserService, UserResolver],
+  providers: [UserService, PatientResolver, PersonnelResolver],
   exports: [UserService],
 })
 export class UserModule {}
