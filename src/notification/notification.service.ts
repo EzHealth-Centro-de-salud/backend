9971e1e22cf115b08a88d0bd91f30e5eb61d10b9
Space_Dog0 <nicolas.bravo@alumnos.ucn.cl>
2024-06-26 19:45:28 -0400

    Final Fix

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -7,11 +7,6 @@ dotenv.config();
 @Injectable()
 export class NotificationService {
   async sendNotification(notif_id: string, title: string, message: string) {
-    console.log('Enviando notificación...');
-    console.log('URL:', process.env.NOTIF_URL);
-    console.log('Sub ID:', notif_id);
-    console.log('App ID:', process.env.NOTIF_ID);
-    console.log('Token:', process.env.NOTIF_TOKEN);
     const url = process.env.NOTIF_URL;
     const payload = {
       subId: notif_id,
@@ -23,7 +18,6 @@ export class NotificationService {
 
     try {
       const response = await axios.post(url, payload);
-      console.log('Notificación enviada:', response.data);
     } catch (error) {
       console.error('Error al enviar notificación:', error);
     }
